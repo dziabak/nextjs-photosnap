6a3dbc975374db7d9e461c91360a13f002eb4548
dziabak <86873665+dziabak@users.noreply.github.com>
2024-01-07 14:20:55 +0100

    apos fix for 'not found' page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
 	title: "Page not found | Photosnap",
-	description: "Page for Photosnap services which is displayed in case the requested page does not exist.",
+	description:
+		"Page for Photosnap services which is displayed in case the requested page does not exist.",
 };
 
 const NotFound = () => {
@@ -11,7 +12,7 @@ const NotFound = () => {
 			<div className="space-y-4 text-center">
 				<h1 className="text-3xl font-bold">Page not found</h1>
 				<p className="text-lg opacity-50">
-					We could not find the page you're looking for.
+					We could not find the page you&apos;re looking for.
 				</p>
 			</div>
 		</main>
