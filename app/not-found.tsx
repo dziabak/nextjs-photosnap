81be16992258835a3c86d023bc47267b6f969646
dziabak <86873665+dziabak@users.noreply.github.com>
2024-01-07 14:16:23 +0100

    Added metadata for 'not-found' page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,18 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Page not found | Photosnap",
+	description: "Page for Photosnap services which is displayed in case the requested page does not exist.",
+};
+
 const NotFound = () => {
 	return (
 		<main>
 			<div className="space-y-4 text-center">
-				<h1 className="text-3xl font-bold">Not found</h1>
-				<p className="text-lg opacity-50">We could not find the page you're looking for.</p>
+				<h1 className="text-3xl font-bold">Page not found</h1>
+				<p className="text-lg opacity-50">
+					We could not find the page you're looking for.
+				</p>
 			</div>
 		</main>
 	);
